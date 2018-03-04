0e4c74981ccb754d2b2b021b123313e0d9c48d44
raddude <jjmarchewitz@gmail.com>
2018-03-04 15:06:49 -0500

    meme

---
+++ JakeRobot2018/src/org/usfirst/frc/team1787/shooting/Intake.java
@@ -0,0 +1,84 @@
+package org.usfirst.frc.team1787.shooting;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/* CLASS DEFINITION:
+ * The intake is defined as the 775 motors attached to the front of the robot and the pneumatics that push the wheels into the cubes
+ */
+
+public class Intake {
+	
+	private final int INTAKE_MASTER_TALON_ID = 4;
+	private final int INTAKE_FOLLOWER_VICTOR_ID = 5;
+	private final int INTAKE_SOLENOID_ID = 2;
+	
+	private WPI_TalonSRX intakeRight = new WPI_TalonSRX(INTAKE_MASTER_TALON_ID);
+	private WPI_VictorSPX intakeLeft = new WPI_VictorSPX(INTAKE_FOLLOWER_VICTOR_ID);
+	
+	private Solenoid intakeSolenoid = new Solenoid(INTAKE_SOLENOID_ID);
+	
+	private static final Intake instance = new Intake();
+	
+	private Intake() {
+		
+		//Configuring motor controllers
+		intakeRight.setInverted(false);
+		intakeLeft.setInverted(false);
+		
+		//Making every talon light up green when forwards
+		intakeRight.configVoltageCompSaturation(12, 10);
+		intakeLeft.configVoltageCompSaturation(12, 10);
+		intakeRight.enableVoltageCompensation(true);
+		intakeLeft.enableVoltageCompensation(true);
+	}
+	
+	
+	
+	public void turnOnWheels(double INTAKE_VOLTAGE) {
+		intakeRight.set(INTAKE_VOLTAGE);
+		intakeLeft.set(INTAKE_VOLTAGE);
+	}
+	
+	public void turnOffWheels() {
+		intakeRight.stopMotor();
+		intakeLeft.stopMotor();
+	}
+	
+	
+	
+	public void squeezeCube() {
+		intakeSolenoid.set(true);
+	}
+
+	public void releaseCube() {
+		intakeSolenoid.set(false);
+	}
+	
+	
+	
+	public void spinCubeLeft(double INTAKE_VOLTAGE) {
+		intakeLeft.set(-INTAKE_VOLTAGE);
+		intakeRight.set(INTAKE_VOLTAGE);
+	}
+	
+	public void spinCubeRight(double INTAKE_VOLTAGE) {
+		intakeLeft.set(INTAKE_VOLTAGE);
+		intakeRight.set(-INTAKE_VOLTAGE);
+	}
+	
+	
+	
+	public void pushDataToShuffleboard() {
+		SmartDashboard.putBoolean("Gripped: ", intakeSolenoid.get());
+		SmartDashboard.putNumber("Right Intake Motor Speed: ", intakeRight.get());
+		SmartDashboard.putNumber("Left Intake Motor Speed: ", intakeLeft.get());
+	}
+	
+	public static Intake getInstance() {
+	    return instance;
+	  }
+
+}
